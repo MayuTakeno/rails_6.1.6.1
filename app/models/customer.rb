67451c6bd6cb200cb5102786e30dbe2a03a061aa
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-14 10:38:11 +0000

    フォロワー機能アソシエーション作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        # フォロワー機能{
+        has_many :relationships, foreign_key: :following_id
+        has_many :followings, through: :relationships, source: :follower
+        has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+        has_many :follower, through: :reverse_of_relationships, source: :following
+        # }
 end
