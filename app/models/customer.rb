648f2df8cf485f64357a876ddca12c059e9a12a8
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-26 08:04:56 +0000

    active_storage追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,10 +4,13 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+        # 画像
+        has_one_attached :image
         # フォロワー機能{
         has_many :relationships, foreign_key: :following_id
         has_many :followings, through: :relationships, source: :follower
         has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
         has_many :follower, through: :reverse_of_relationships, source: :following
         # }
+
 end
