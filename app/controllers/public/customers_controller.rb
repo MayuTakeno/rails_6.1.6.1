50fa7b43679aded9daf3e8385f11ece07c694740
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-10-26 08:00:59 +0000

    params記述

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit]
   def index
     @customers = Customer.all
   end
@@ -7,5 +8,15 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def set_customer
+    @custmer = Customer.find(params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :first_kana, :last_kana, :phone, :post_code, :address, :age, :is_deleted)
   end
 end
